49f69ae78f98d660389d232b4e0ad32f179f7980
tajimata <tajimax0107@gmail.com>
2019-10-04 16:25:29 +0900

    投稿機能＆ログイン機能

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
-    @racket = Racket.all
+    # @racket = Racket.find(3)
+    @racket = Racket.all.order("id DESC").page(params[:page]).per(5)
+    # binding.pry
   end
 
   def new
@@ -13,5 +17,9 @@ class HomeController < ApplicationController
   private
   def racket_params
     params.permit(:name, :image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root  'home#index' 
   get 'home' => 'home#index'
   get 'edit' => 'edit#edit'
   get   'home/new'  =>  'home#new'
