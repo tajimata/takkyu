5e3935e731bd6d96bb9ebe433a8828ecd6daab8c
tajimata <tajimax0107@gmail.com>
2019-10-06 14:24:28 +0900

    削除、編集機能実装

--- app/models/racket.rb
+++ app/models/racket.rb
@@ -1,2 +1,3 @@
 class Racket < ApplicationRecord
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   get 'edit' => 'edit#edit'
   get   'home/new'  =>  'home#new'
   post  'home'      =>  'home#create'
+  delete  'home/:id'  => 'home#destroy'
+  get   'home/:id/edit'  => 'home#edit'
+  patch   'home/:id'  => 'home#update'
+  get   'users/:id'   =>  'users#show'  #Mypageへのルーティング
+  get 'tweets/:id' => 'tweets#show'  
 end
